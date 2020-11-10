8f69dc6eddbc9360d0923327a1d85dfb61bfba40
elssm <329847986@qq.com>
2020-11-10 14:45:20 +0800

    third submit

--- src/main.js
+++ src/main.js
@@ -18,18 +18,26 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource)
 //设置请求的根路径
 Vue.http.options.root = 'http://www.liulongbin.top:3005/';
+//全局设置post时候，表单数据的组织形式
+Vue.http.options.emulateJSON = true;
 
 //按需导入MintUI中的组件
-import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
-Vue.component(Header.name,Header)
-Vue.component(Swipe.name, Swipe)
-Vue.component(SwipeItem.name, SwipeItem)
-Vue.component(Button.name, Button)
+// import { Header, Swipe, SwipeItem, Button,Lazyload } from 'mint-ui'
+// Vue.component(Header.name,Header)
+// Vue.component(Swipe.name, Swipe)
+// Vue.component(SwipeItem.name, SwipeItem)
+// Vue.component(Button.name, Button)
+
+// Vue.use(Lazyload);
+import MintUI from 'mint-ui'
+Vue.use(MintUI);
+import 'mint-ui/lib/style.css'
 
 
 //导入MUI样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
+// import './lib/mui/js/mui.js'
 
 //1.3导入自己的router.js路由模块
 import router from './router.js'
