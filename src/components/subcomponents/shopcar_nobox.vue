ea359dab3177825578651cac2eab6e27ff61b407
elssm <329847986@qq.com>
2020-11-17 22:42:53 +0800

    submit

---
+++ src/components/subcomponents/shopcar_nobox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1' style="height: 25px;">
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <input id="test" class="mui-input-numbox" type="number" value="1" ref="nobox" @change='countChanged'/>
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+		</div>
+</template>
+
+<script>
+//导入mui从而支持初始化数字框
+import mui from '../../lib/mui/js/mui.js'
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        //当组件挂在到页面中之后，去初始化数字框
+        mui('.mui-numbox').numbox()
+    },
+    methods: {
+        countChanged(){
+            //获取选择的商品数量
+            const val = this.$refs.nobox.value;
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/tabber/ShopcarContainer.vue
+++ src/components/tabber/ShopcarContainer.vue
@@ -1,15 +1,102 @@
 <template>
-    <div>
-        <h3>ShopcarContainer</h3>
+    <div class="shopcar-container">
+        <div class="goods-list">
+
+            <div class="mui-card">
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner goods-item">
+                        <!--开关-->
+						<mt-switch></mt-switch>
+
+                        <!--图片-->
+                        <img src="../../images/menu1.png" alt="">
+                        <div class="info">
+                            <h1>小米Note 16G手机双网通版本</h1>
+                            <div class="goods-info">
+                                <span class="price">¥2199</span>
+                                <nobox></nobox>
+                                <a href="">删除</a>
+                            </div>
+                        </div>
+					</div>
+				</div>
+			</div>
+
+            <!--结算区域-->
+            <div class="mui-card">
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner jiesuan">
+                        <div class="left">
+                            <p>总计（不含运费）</p>
+                            <p>
+                                已勾选商品<span class="danger">0</span>件，总价<span class="danger">¥0</span>
+                            </p>
+                        </div>
+                        <mt-button type="danger">去结算</mt-button>
+					</div>
+				</div>
+			</div>
+        </div>
     </div>
 </template>
 
 <script>
+import nobox from '../subcomponents/shopcar_nobox.vue'
 export default {
-    
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    components:{
+        nobox
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    
+.shopcar-container{
+    background-color: #eee;
+    overflow: hidden;
+
+    .goods-item{
+        display: flex;
+    img{
+        width: 60px;
+        height: 60px;
+    }
+    h1{
+        font-size: 13px;
+    }
+    .info{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .goods-info{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            
+            .price{
+                color: red;
+                font-weight: bold;
+                font-size: 16px;
+            }
+        }
+    }
+    }
+}
+.danger{
+    color: red;
+    font-weight: bold;
+    font-size: 16px;
+}
+.jiesuan{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 </style>
